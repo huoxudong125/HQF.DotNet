716603aa443978d835c60ad8018594b5f31fde04
bshn <bshn@FRANK-PC>
2016-07-12 15:22:45 +0800

    添加项目文件。

---
+++ HQF.DotNet.Common/CodeTimer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HQF.DotNet.Common
+{
+    public static class CodeTimer
+    {
+        public static void Initialize()
+        {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            Time("", 1, () => { });
+        }
+
+        public static void Time(string name, int iteration, Action action)
+        {
+            if (String.IsNullOrEmpty(name)) return;
+
+            // 保留当前控制台前景色，并使用黄色输出名称参数。
+            ConsoleColor currentForeColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(name);
+
+            // 强制GC进行收集，并记录目前各代已经收集的次数。
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            int[] gcCounts = new int[GC.MaxGeneration + 1];
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                gcCounts[i] = GC.CollectionCount(i);
+            }
+
+            // 执行代码，记录下消耗的时间。
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < iteration; i++) action();
+            watch.Stop();
+
+            // 恢复控制台默认前景色，并打印出消耗时间。
+            Console.ForegroundColor = currentForeColor;
+            Console.WriteLine("\tTime Elapsed:\t" + watch.ElapsedMilliseconds.ToString("N0") + "ms");
+
+            // 打印执行过程中各代垃圾收集回收次数。
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                int count = GC.CollectionCount(i) - gcCounts[i];
+                Console.WriteLine("\tGen " + i + ": \t\t" + count);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
